13e09e4642ca16c9cad05c79c55f85dfa281a6c9
urvish.rana <urvish.rana@volansystech.com>
2018-12-17 18:13:42 +0530

    Android Application code for Geo Fencing.
    Signed-off-by: urvish.rana <urvish.rana@volansystech.com>

---
+++ ContactMe/app/src/main/java/com/example/volansys/contactme/ContactFragment.java
@@ -0,0 +1,88 @@
+package com.example.volansys.contactme;
+
+
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class ContactFragment extends Fragment implements OnMapReadyCallback {
+    MapView mapView;
+    GoogleMap googleMap;
+    private WebView webView;
+    public ContactFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_contact,container,false);
+        getActivity().setTitle(R.string.contact_us);
+
+        mapView=view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+        return view;
+    }
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap=googleMap;
+        googleMap.setMinZoomPreference(12);
+        UiSettings uiSettings = googleMap.getUiSettings();
+        uiSettings.setIndoorLevelPickerEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setMapToolbarEnabled(true);
+        uiSettings.setCompassEnabled(true);
+        uiSettings.setZoomControlsEnabled(true);
+        LatLng gujrat=new LatLng(23.010819, 72.506441);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(gujrat);
+        googleMap.addMarker(markerOptions);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(gujrat));
+    }
+
+}
---
+++ ContactMe/app/src/main/java/com/example/volansys/contactme/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.volansys.contactme;
+
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container_wrapper,new ContactFragment());
+        fragmentTransaction.commit();
+    }
+}
